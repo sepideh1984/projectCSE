95751fc8c2905bc129f9ed7c2e8bf2e666d26ce3
sepideh1984 <92688133+sepideh1984@users.noreply.github.com>
2021-10-17 20:32:24 +0200

    Add files via upload

---
+++ toggel/toggel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toggel',
+  templateUrl: './toggel.component.html',
+  styleUrls: ['./toggel.component.css']
+})
+export class ToggelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
